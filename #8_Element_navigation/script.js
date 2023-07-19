e80fea082b60073355419a9372f339874002cbd6
Sudhakar <sidhu1171bansal@gmail.com>
2023-07-19 13:05:11 +0530

    Navigating only Elemts

---
+++ #8_Element_navigation/script.js
@@ -0,0 +1,7 @@
+const chngbg = () =>{
+    document.body.firstElementChild.style.background = "red";
+}
+b= document.body;
+console.log("first child of body is : ",b.firstChild);
+console.log("first child of body is : ",b.firstElementChild);
+// type chngbg on console
