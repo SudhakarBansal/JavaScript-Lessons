c21d87b665be9227bb8c987501f4036e9d352ab2
Sudhakar <sidhu1171bansal@gmail.com>
2023-08-10 16:45:32 +0530

    Inserting elements in javaScript

---
+++ #10_insertion_in_HTML/script.js
@@ -0,0 +1,5 @@
+first = document.getElementById("first");
+first.insertAdjacentHTML("beforebegin", "<div>beforebegin.. </div>");
+first.insertAdjacentHTML("beforeend", "<div>beforeend.. </div>");
+first.insertAdjacentHTML("afterbegin", "<div>afterbegin.. </div>");
+first.insertAdjacentHTML("afterend", "<div>afterend.. </div>");
